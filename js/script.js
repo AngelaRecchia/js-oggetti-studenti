b1f017ba9a5c702cddc69b4b7c94c88d1a4b7449
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-06-14 14:26:57 +0200

    oggetto studente

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 Creare un array di oggetti di studenti.
 Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
+
+// crea oggetto studente e ne stampa le proprietà
+var studente = {
+    nome: "Pinco",
+    cognome: "Bianchi",
+    eta: "20"
+};
+
+for (var key in studente){
+    console.log(studente[key]);
+}
